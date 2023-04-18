b8b4ed3c9b5ca2c4e049b75f5fbf20186ec72d4f
Elia Geretto <elia.f.geretto@gmail.com>
2023-04-18 12:02:10 +0200

    Add C++ instrumentation test

---
+++ test/harness.cpp
@@ -0,0 +1,52 @@
+// RUN: echo 'test/harness.cpp:39' > %t.targets.txt
+// RUN: AFLGO_TARGETS=%t.targets.txt %libaflgo_cxx_test %s -o %t -Wl,-save-temps
+// RUN: llvm-dis-16 %t.0.5.precodegen.bc
+// RUN: cat %t.0.5.precodegen.ll | %FileCheck %s
+
+#include <cstdint>
+#include <cstdlib>
+#include <memory>
+
+class Base {
+public:
+  virtual int getRetVal() = 0;
+  virtual ~Base() {}
+};
+
+class Sub1 : public Base {
+  int getRetVal() override {
+    return 1;
+  }
+};
+
+class Sub2 : public Base {
+public: 
+  int getRetVal() override {
+// CHECK: define {{.*}} i32 @_ZN4Sub29getRetValEv
+// CHECK-NEXT: entry:
+// CHECK-NEXT: call void @__aflgo_trace_bb_distance(double 1.000000e+01)
+    int Ret = computeRetVal();
+// CHECK: call void @__sanitizer_cov_trace_pc_guard
+    return Ret;
+  }
+
+private:
+// CHECK: define {{.*}} void @_ZN4Sub213computeRetValEv
+  int computeRetVal() {
+// CHECK-NEXT: entry:
+// CHECK-NEXT: call void @__aflgo_trace_bb_distance(double 0.000000e+00)
+// CHECK: call void @__sanitizer_cov_trace_pc_guard
+    return 2;
+  }
+};
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+  std::unique_ptr<Base> B = nullptr;
+  if (Size > 5) {
+    B = std::make_unique<Sub1>();
+  } else {
+    B = std::make_unique<Sub2>();
+  }
+
+  return B->getRetVal();
+}
