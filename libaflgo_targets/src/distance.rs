1598d339368aa00b536158fdd07106a29f065c96
Elia Geretto <elia.f.geretto@gmail.com>
2023-05-15 09:35:37 +0000

    feat(libaflgo_targets): Use atomics for distance calculation

--- libaflgo_targets/src/distance.rs
+++ libaflgo_targets/src/distance.rs
@@ -1,45 +1,55 @@
-use once_cell::sync::Lazy;
-use std::sync::Mutex;
+use std::sync::{atomic::AtomicU64, atomic::Ordering};
+
+const DISTANCE_RESOLUTION: f64 = 1000_f64;
 
 #[derive(Default)]
 struct DistanceStats {
-    bb_distance_sum: f64,
-    bb_distance_count: u64,
+    bb_distance_sum: AtomicU64,
+    bb_distance_count: AtomicU64,
 }
 
 impl DistanceStats {
-    pub fn add_bb_distance(&mut self, bb_distance: f64) {
-        self.bb_distance_sum += bb_distance;
-        self.bb_distance_count += 1;
+    pub const fn new() -> Self {
+        Self {
+            bb_distance_sum: AtomicU64::new(0),
+            bb_distance_count: AtomicU64::new(0),
+        }
+    }
+
+    pub fn add_bb_distance(&self, bb_distance: f64) {
+        let distance_approx = (bb_distance * DISTANCE_RESOLUTION).trunc() as u64;
+
+        self.bb_distance_sum
+            .fetch_add(distance_approx, Ordering::Relaxed);
+        self.bb_distance_count.fetch_add(1, Ordering::Relaxed);
     }
 
     pub fn compute_test_case_distance(&self) -> f64 {
-        self.bb_distance_sum / self.bb_distance_count as f64
+        let distance_restored =
+            self.bb_distance_sum.load(Ordering::Relaxed) as f64 / DISTANCE_RESOLUTION;
+        distance_restored / self.bb_distance_count.load(Ordering::Relaxed) as f64
     }
 
-    pub fn reset(&mut self) {
-        self.bb_distance_sum = 0.0;
-        self.bb_distance_count = 0;
+    pub fn reset(&self) {
+        self.bb_distance_sum.store(0, Ordering::Relaxed);
+        self.bb_distance_count.store(0, Ordering::Relaxed);
     }
 }
 
-static STATS: Lazy<Mutex<DistanceStats>> = Lazy::new(|| Mutex::new(DistanceStats::default()));
+static STATS: DistanceStats = DistanceStats::new();
 
 // Called by the distance instrumentation
 #[no_mangle]
 pub extern "C" fn __aflgo_trace_bb_distance(bb_distance: f64) {
-    let mut stats = STATS.lock().unwrap();
-    stats.add_bb_distance(bb_distance);
+    STATS.add_bb_distance(bb_distance);
 }
 
 pub fn compute_test_case_distance() -> f64 {
-    let stats = STATS.lock().unwrap();
-    stats.compute_test_case_distance()
+    STATS.compute_test_case_distance()
 }
 
 pub fn reset_distance_stats() {
-    let mut stats = STATS.lock().unwrap();
-    stats.reset();
+    STATS.reset();
 }
 
 #[cfg(test)]
