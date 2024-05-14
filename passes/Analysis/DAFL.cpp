38949f528fb4a8b1d43d6cc06222ebd261c2e201
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2024-05-14 10:44:26 +0200

    fix(passes/DAFL): fix order of iteration

--- passes/Analysis/DAFL.cpp
+++ passes/Analysis/DAFL.cpp
@@ -9,6 +9,7 @@
 #include "WPA/Andersen.h"
 
 #include <llvm/ADT/SmallSet.h>
+#include <llvm/ADT/SmallVector.h>
 #include <llvm/IR/BasicBlock.h>
 #include <llvm/IR/Constants.h>
 #include <llvm/IR/Instructions.h>
@@ -132,7 +133,7 @@ DAFLAnalysis::Result DAFLAnalysis::run(Module &M, ModuleAnalysisManager &MAM) {
   }
 
   // get the target instructions
-  SmallSet<const Instruction *, 32> TargetIs;
+  SmallSetVector<const Instruction *, 32> TargetIs;
   auto &FAM = MAM.getResult<FunctionAnalysisManagerModuleProxy>(M).getManager();
   for (auto &F : M) {
     auto &FTargets = FAM.getResult<AFLGoTargetDetectionAnalysis>(F);
@@ -181,14 +182,14 @@ DAFLAnalysis::Result DAFLAnalysis::run(Module &M, ModuleAnalysisManager &MAM) {
   // First remove intrinsics because we may end up adding to the target
   // instruction set some values that are arguments to stuff like lifetime
   // management intrinsics.
-  SmallVector<const Instruction *, 32> TargetsToRemove;
-  for (const auto *I : TargetIs) {
-    if (const auto *II = dyn_cast<IntrinsicInst>(I)) {
+  SmallSetVector<const Instruction *, 32> TargetsToRemove;
+  for (auto I = TargetIs.rbegin(), E = TargetIs.rend(); I != E; ++I) {
+    if (const auto *II = dyn_cast<IntrinsicInst>(*I)) {
       auto IID = II->getIntrinsicID();
       if (IID == Intrinsic::lifetime_start || IID == Intrinsic::lifetime_end ||
           IID == Intrinsic::dbg_declare || IID == Intrinsic::dbg_value ||
           IID == Intrinsic::dbg_label || IID == Intrinsic::dbg_addr) {
-        TargetsToRemove.push_back(I);
+        TargetsToRemove.insert(II);
         // add arguments to the intrinsic instruction
         for (const auto &Op : II->operands()) {
           if (const auto *OpI = dyn_cast<Instruction>(Op)) {
@@ -211,7 +212,7 @@ DAFLAnalysis::Result DAFLAnalysis::run(Module &M, ModuleAnalysisManager &MAM) {
               }
             }
             if (HasNoOtherUser) {
-              TargetsToRemove.push_back(OpI);
+              TargetsToRemove.insert(OpI);
             }
           }
         }
@@ -219,9 +220,7 @@ DAFLAnalysis::Result DAFLAnalysis::run(Module &M, ModuleAnalysisManager &MAM) {
     }
   }
 
-  for (const auto *I : TargetsToRemove) {
-    TargetIs.erase(I);
-  }
+  TargetIs.set_subtract(TargetsToRemove);
 
   // If the target is a call instruction, with optimization enabled, it may
   // happen that it is the only instruction that corresponds to the target line
@@ -229,61 +228,55 @@ DAFLAnalysis::Result DAFLAnalysis::run(Module &M, ModuleAnalysisManager &MAM) {
   // SVFG does not contain nodes for call instructions. In this case it's safe
   // to add the arguments to the call to the target instructions set.
   // The same holds for return instructions and conditional branches.
-  TargetsToRemove.clear();
-  SmallVector<const Instruction *, 32> TargetsToAdd;
-  for (const auto *I : TargetIs) {
+  SmallSetVector<const Instruction *, 32> Worklist(TargetIs.begin(),
+                                                   TargetIs.end());
+  TargetIs.clear();
+  while (!Worklist.empty()) {
+    auto *I = Worklist.pop_back_val();
     if (const auto *CB = dyn_cast<CallBase>(I)) {
-      TargetsToRemove.push_back(I);
+      TargetsToRemove.insert(I);
       for (const auto &Arg : CB->args()) {
         if (const auto *ArgI = dyn_cast<Instruction>(Arg)) {
-          TargetsToAdd.push_back(ArgI);
+          Worklist.insert(ArgI);
         }
       }
     } else if (const auto *RI = dyn_cast<ReturnInst>(I)) {
-      TargetsToRemove.push_back(I);
+      TargetsToRemove.insert(I);
       if (const auto *RV = RI->getReturnValue()) {
         if (const auto *RVI = dyn_cast<Instruction>(RV)) {
-          TargetsToAdd.push_back(RVI);
+          Worklist.insert(RVI);
         }
       }
     } else if (const auto *BI = dyn_cast<BranchInst>(I)) {
       if (BI->isConditional()) {
-        TargetsToRemove.push_back(I);
         if (const auto *CI = dyn_cast<Instruction>(BI->getCondition())) {
-          TargetsToAdd.push_back(CI);
+          Worklist.insert(CI);
         }
       }
+    } else {
+      TargetIs.insert(I);
     }
   }
 
-  for (const auto *I : TargetsToAdd) {
-    TargetIs.insert(I);
-  }
-  for (const auto *I : TargetsToRemove) {
-    TargetIs.erase(I);
-  }
-
-  // skip intrinsics and instructions that are not in SVFG
+  // skip remaining instructions that are not in SVFG
   TargetsToRemove.clear();
   for (const auto *I : TargetIs) {
     if (auto *BI = dyn_cast<BranchInst>(I)) {
       if (BI->isUnconditional()) {
         errs() << "Skipping unconditional branch target inst: " << *I << "\n";
-        TargetsToRemove.push_back(I);
+        TargetsToRemove.insert(I);
       }
     }
   }
 
-  for (const auto *I : TargetsToRemove) {
-    TargetIs.erase(I);
-  }
+  TargetIs.set_subtract(TargetsToRemove);
 
   if (TargetIs.empty()) {
     report_fatal_error("No target instructions left after filtering");
   }
 
   // track which instructions are present in SVFG
-  decltype(TargetIs) SeenTargetIs;
+  SmallSet<const Instruction *, 32> SeenTargetIs;
 
   // adjacency list from node to its defs
   std::map<NodeID, std::vector<Edge>> G;
